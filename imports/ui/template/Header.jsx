85b33e88bccf5a75846a8dcff5dcee4471765b0d
Jacob White <jacob@jbuc.com>
2016-10-22 17:42:04 -0500

    organize sitewide css

--- imports/ui/template/Header.jsx
+++ imports/ui/template/Header.jsx
@@ -5,24 +5,7 @@ import { Link } from 'react-router';
 export default class Header extends Component {
   render() {
     return (
-        <div className="top-bar">
-            <div className="row">
-                <div className="top-bar-left">
-                    <ul className="dropdown menu" data-dropdown-menu>
-                        <li className="menu-text">Samaritan</li>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="about">About</Link></li>
-                        <li><Link to="nomatch">No Match</Link></li>
-                    </ul>
-                </div>
-                <div className="top-bar-right">
-                    <ul className="menu">
-                    <li><input type="search" placeholder="Search" /></li>
-                    <li><button type="button" className="button">Search</button></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <div className="top-bar"></div>
     );
   }
 }
