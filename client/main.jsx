ec15395332ead9a40fc7f7e2c66baca9214b4f94
Ehren Coker <mrcoker@gmail.com>
2016-10-23 04:03:34 -0500

    getting ready for the big show

--- client/main.jsx
+++ client/main.jsx
@@ -8,6 +8,7 @@ import Home from '../imports/ui/pages/Home.jsx';
 import Form from '../imports/ui/pages/Form.jsx';
 import About from '../imports/ui/pages/About.jsx';
 import Clients from '../imports/ui/pages/Clients.jsx';
+import Samaritan from '../imports/ui/pages/Samaritan.jsx';
 import Shelters from '../imports/ui/pages/Shelters.jsx';
 import NoMatch from '../imports/ui/pages/NoMatch.jsx';
 
@@ -19,6 +20,7 @@ Meteor.startup(() => {
       <Route path="about" component={About}/>
       <Route path="clients" component={Clients}/>
       <Route path="clients/shelters/:id" component={Shelters}/>
+      <Route path="samaritan" component={Samaritan}/>
       <Route path="*" component={NoMatch}/>
     </Router>
   ), document.getElementById('render-target'))
--- imports/ui/pages/Clients.jsx
+++ imports/ui/pages/Clients.jsx
@@ -16,17 +16,27 @@ class ClientPage extends Component {
       this.renderClients = this.renderClients.bind(this);
   }
   cleanString(str) {
-      if (str || typeof str == 'string') {
+      if (str && typeof str == 'string') {
           return str.replace('+', '').replace(/^on$/i,'✓');
       } else {
-          return str;
+          return '';
       }
   }
   renderClients(){
-    return this.props.clients.map((client) => (
-        <tr key={client._id}>
-            <td>{ this.cleanString( client.firstName ) } { this.cleanString( client.lastName ) }</td>
-            <td>{ this.cleanString( client.financialNeed ) }</td>
+    return this.props.clients.map((client, i) => (
+        <tr key={`client-${i}`}>
+            <td className="name-cell">{ this.cleanString( client.firstName ) } { this.cleanString( client.middleName ) } { this.cleanString( client.lastName ) }</td>
+            <td>{ this.cleanString( client.dateOfBirth) }</td>
+            <td>{ this.cleanString( client.gender ) }</td>
+            <td className="centered">{ this.cleanString( client.safetyNeed ) }</td>
+            <td className="centered">{ this.cleanString( client.healthNeed ) }</td>
+            <td className="centered">{ this.cleanString( client.foodNeed ) }</td>
+            <td className="centered">{ this.cleanString( client.financialNeed ) }</td>
+            <td className="centered">{ this.cleanString( client.isVeteran ? 'on' : '' ) }</td>
+            <td>{ this.cleanString( client.veteranOf ) }</td>
+            <td>{ this.cleanString( client.serviceStart ) }</td>
+            <td>{ this.cleanString( client.serviceEnd ) }</td>
+            <td>{ this.cleanString( client.socialSecurity ) }</td>
         </tr>
     ));
   }
@@ -40,13 +50,25 @@ class ClientPage extends Component {
       this.renderClients();
     return (
         <div className="container">
-            <Header />
-            <div className="row column">
-                <table>
+            <div className="form-part-header">
+                <p>Client List</p>
+            </div>
+            <div className="row column all-the-overflow">
+                <table className="nowrap-table">
                     <thead>
                         <tr>
                             <th>Name</th>
+                            <th>Date of Birth</th>
+                            <th>Gender</th>
+                            <th>Safety Need</th>
+                            <th>Health Need</th>
+                            <th>Food Need</th>
                             <th>Financial Need</th>
+                            <th>Veteran Status</th>
+                            <th>Veteran Of</th>
+                            <th>Service Start</th>
+                            <th>Service End</th>
+                            <th>SSN</th>
                         </tr>
                     </thead>
                     <tbody>
