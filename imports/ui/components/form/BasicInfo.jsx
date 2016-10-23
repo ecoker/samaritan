57431b30daf706792850874a714bd57d33b1a7f3
Jacob White <jacob@jbuc.com>
2016-10-22 19:07:32 -0500

    Military Page

--- imports/ui/components/form/BasicInfo.jsx
+++ imports/ui/components/form/BasicInfo.jsx
@@ -38,15 +38,16 @@ export default class BasicInfo extends Component {
                     </div>
                     <fieldset className="columns small-12">
                         <legend>Gender</legend> 
-                        <label htmlFor="genderMale" className="columns medium-4">
+                        <label htmlFor="genderMale" className="columns small-3">
                             <input type="radio" name="gender" value="male" id="genderMale" /> Male
                         </label>
-                        <label htmlFor="genderFemale" className="columns medium-4">
+                        <label htmlFor="genderFemale" className="columns small-3">
                             <input type="radio" name="gender" value="female" id="genderFemale" /> Female
                         </label>
-                        <label htmlFor="genderCustom" className="columns medium-4">
+                        <label htmlFor="genderCustom" className="columns small-3">
                             <input type="radio" name="gender" value="custom" id="genderCustom" /> Custom
                         </label>
+                        <div className="columns small-3"></div>
                     </fieldset>
                 </div>
             </section>
--- imports/ui/components/form/MilitaryService.jsx
+++ imports/ui/components/form/MilitaryService.jsx
@@ -4,19 +4,48 @@ export default class BasicInfo extends Component {
   render() {
     return (
         <div>
-            <h2>Military Service</h2>
-            <div className="name-field">
-                <label>First Name <small>required</small>
-                <input name="firstName" type="text" required pattern="[a-zA-Z]+" />
-                </label>
-                <small className="error">Name is required and must be a string.</small>
+            <h2 className="coach">“We need some info about your military service.”</h2>
+            <div className="row">
+                <div className="small-6 columns">
+                    <label htmlFor="serviceStart">Service Start
+                        <input name="serviceStart" type="text" pattern="[\d]{4}" placeholder="YYYY" />
+                    </label>
+                </div>
+                <div className="small-6 columns">
+                    <label htmlFor="serviceEnd">Service End
+                        <input name="serviceEnd" type="text" pattern="[\d]{4}" placeholder="YYYY" />
+                    </label>
+                </div>
+                <div className="small-12 columns">
+                    <label htmlFor="veteranOf">Veteran Of
+                        <select name="veteranOf">
+                            <option value=""></option>
+                            <option value="WorldWarII">World War II</option>
+                            <option value="KoreanWar">Korean War</option>
+                            <option value="VietnamWar">Vietnam War</option>
+                            <option value="DesertStorm">Desert Storm</option>
+                            <option value="AfghanistanOEF">Afghanistan</option>
+                            <option value="IraqOIF">Iraq OIF</option>
+                            <option value="IraqOND">Iraq OND</option>
+                            <option value="OtherTheater">Other Theater</option>
+                        </select>
+                    </label>
+                </div>
+                <div className="small-12 columns">
+                    <label htmlFor="veteranOf">Disharge Status
+                        <select name="veteranOf">
+                            <option value="0"></option>
+                            <option value="1">Honorable Discharge</option>
+                            <option value="2">Other Than Honorable Conditions Discharge</option>
+                            <option value="3">Bad Conduct Discharge (BCD)</option>
+                            <option value="4">Dishonorable Discharge</option>
+                            <option value="5">Officer Discharge</option>
+                            <option value="6">Entry Level Separation (ELS)</option>
+                        </select>
+                    </label>
+                </div>
             </div>
-            <div className="name-field">
-                <label>Last Name <small>required</small>
-                <input name="lastName" type="text" required pattern="[a-zA-Z]+" />
-                </label>
-                <small className="error">Name is required and must be a string.</small>
-            </div>
+
         </div>
     );
   }
